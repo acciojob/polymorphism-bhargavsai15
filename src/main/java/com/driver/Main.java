49759b4ddf5832028e73cfe21c9d8668123bb115
bharavsai15 <bhargavcme123@gmail.com>
2022-12-10 19:03:22 +0530

    message

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -1,5 +1,18 @@
 package com.driver;
 
 public class Main {
+    public  static class Product{
+        public int product(int x, int y) {}
+        public int product(int x, int y, int z) {}
 
+        public double product(double x, double y) {}
+    }
+
+    public static void main(String[] args) {
+        Product p=new Product();
+        p.product(1,2);
+        p.product(1,2,3);
+        p.product(1.0,2.0);
+
+    }
 }
